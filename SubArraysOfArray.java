b06adb8804e7943337100390abe898fee1578527
Rupam <rrai838@gmail.com>
2021-10-17 20:15:54 -0500

    Print SubArray Of An Array

---
+++ SubArraysOfArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SubArraysOfArray{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+    
+        for(int i = 0; i < size; i++){
+            arr[i] = sc.nextInt();
+        }
+    
+        for(int i = 0; i < size; i++){
+            for(int j = i; j < size; j++){
+                for(int k = i; k <= j; k++){
+                    System.out.print(arr[k] + "\t");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
