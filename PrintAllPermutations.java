f9725ed0c949559a9afb477ea7367f7d4ac467f6
Rupam <rrai838@gmail.com>
2021-11-08 20:36:32 -0600

    Print String Permutations

---
+++ PrintAllPermutations.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PrintAllPermutations {
+
+	public static void permutations(String str){
+		int n = str.length();
+		int f = factorial(n);
+		
+		for(int i = 0; i < f; i++){
+		    StringBuilder sb = new StringBuilder(str); // Need Every Time 
+		    int temp = i;
+		    for(int div = n; div >= 1; div--){
+		        int q = temp / div;
+		        int r = temp % div;
+		        
+		        System.out.print(sb.charAt(r));
+		        sb.deleteCharAt(r);
+		        temp = q;
+		    }
+		    System.out.println();
+		}
+		
+	}
+	
+	public static int factorial(int n){
+	    int val = 1;
+	    
+	    for(int i = 2; i <= n ; i++){
+	        val *= i;
+	    }
+	    
+	    return val;
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		permutations(str);
+	}
+
+}
